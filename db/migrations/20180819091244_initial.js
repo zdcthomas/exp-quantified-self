fe5ab9e526f9f2fd3af23aaae51d6f696581e755
zdcthomas <zdcthomas@yahoo.com>
2018-08-19 09:34:58 -0600

    creates dev db and initial migrations

---
+++ db/migrations/20180819091244_initial.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('foods', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('calories');
+
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('meals', (table)=>{
+      table.increments('id').primary();
+      table.string('name')
+    }),
+    knex.schema.createTable('meal_foods', (table)=>{
+      table.increments('id').primary();
+      table.integer('food_id').references('foods.id')
+      table.integer('meals_id').references('meals.id')
+    })
+  ])
+  
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('foods')
+  ]);
+};
