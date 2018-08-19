225f567daaf2b78b62dc847cc36b05fa4b27b042
zdcthomas <zdcthomas@yahoo.com>
2018-08-19 13:27:50 -0600

    tests get foods endpoint

---
+++ routes/food.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('.../knexfile')[environment];
+// const database = require('knex')(configuration);
+
+router.get('/api/v1/foods', ()=>{
+  
+})
+
+module.exports = router;
--- test/foods.spec.js
+++ test/foods.spec.js
@@ -1,9 +1,54 @@
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
+const food_controller = require('../bin/www');
+
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
 
 chai.use(chaiHttp);
 
+before((done) => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+    .then( () => {
+      database('foods').insert({name:"bannana", calories: 150}, 'id')
+      database('foods').insert({name:"apple", calories: 200}, 'id')
+      database('foods').insert({name:"pear", calories: 50}, 'id')
+    })
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+
 describe('API Route end points', () => {
-  
+  describe('get api/v1/foods', () => {
+    it('should return all food entries in the database', ()=> {
+      chai.request(food_controller)
+      .get('/api/v1/foods')
+      .end( (err, response) => {
+        response.should.have.status(200)
+        response.should.be.json;
+        response.should.be.a('array')
+        response.body.count.should.equal(3)
+        response.body[0].should.have.property('name')
+        response.body[0].should.have.property('calories')        
+        response.body[0].should.have.property('id')
+        response.body[0].name.should.equal('bannana')
+        response.body[0].calories.should.equal(150)
+        console.log('hello')
+        done();
+      })
+    })
+  });
 });
