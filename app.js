0d9ea30e24aab2b96c3f30c83533d7d6da7b2f12
zdcthomas <zdcthomas@yahoo.com>
2018-08-19 23:46:02 -0600

    trying changing the routing in app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/foods', foodRouter);
+app.use('/api/v1/foods/', foodRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
