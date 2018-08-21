80ca866f5276edc9bb70440c22d7a1077c5ebd22
zdcthomas <zdcthomas@yahoo.com>
2018-08-20 22:01:25 -0600

    implements post meal foods

--- routes/meal.js
+++ routes/meal.js
@@ -11,7 +11,7 @@ const database = require('knex')(configuration);
 router.use(cors());
 
 router.get('/', cors(), async(request, response, next)=>{
-  
+
   let meals = await database('meals').select()
   meals = await Promise.all(meals.map(async(meal)=>{
                 let foods = await Meal.foods(meal.id)
@@ -22,7 +22,28 @@ router.get('/', cors(), async(request, response, next)=>{
   if (meals[0].name) {
     response.status(200).json(meals)
   } else {
-    response.status(500).json({error})
+    response.status(500).json({error:"internal server error"})
   }
 })
+
+router.post('/:meal_id/foods/:food_id', cors(), async(request, response, next)=>{
+  let meal_id = request.params.meal_id
+  let food_id = request.params.food_id
+  let meal = await database('meals').where({id:meal_id}).select()
+  let food = await database('foods').where({id:food_id}).select()
+  if (meal[0] && food[0]) {
+    database('meal_foods').insert({meal_id:meal[0].id, food_id:food[0].id})
+    .then( ()=>{
+        response.status(201).json({message:`Successfully added ${food[0].name} to ${meal[0].name}`})
+    })
+    .catch(error=>{
+      response.status(500).json({error})
+    })
+  } else {
+    response.status(404).json({error:"Meal or Food not found"})
+
+  }
+  
+})
+
 module.exports = router;
--- test/meals.spec.js
+++ test/meals.spec.js
@@ -12,41 +12,43 @@ const database = require('knex')(configuration);
 
 chai.use(chaiHttp);
 
-before((done) => {
-database.migrate.latest()
-  .then(() => done())
-  .catch(error => {
-    throw error;
-  });
-});
 
-beforeEach((done) => {
-  database.seed.run()
-  .then( () => {
-    return Promise.all([
-      database('foods').insert({name:"bannana", calories: 150, id:1}),
-      database('foods').insert({name:"apple", calories: 200, id:2}),
-      database('foods').insert({name:"pear", calories: 50, id:3}),
-      database('foods').insert({name:"bagel", calories: 400, id:4})
-    ])
-  })
-  .then( ()=>{
-    return Promise.all([
-      database('meal_foods').insert({meal_id:1, food_id:1}, 'id'),
-      database('meal_foods').insert({meal_id:1, food_id:2}, 'id'),
-      database('meal_foods').insert({meal_id:2, food_id:2}, 'id'),
-      database('meal_foods').insert({meal_id:2, food_id:3}, 'id'),
-      database('meal_foods').insert({meal_id:4, food_id:1}, 'id'),
-      database('meal_foods').insert({meal_id:4, food_id:2}, 'id'),
-    ])
-  })
-  .then(() => done())
-  .catch(error => {
-    throw error;
-  });
-});
 
 describe('meal requests', ()=>{
+
+  before((done) => {
+    database.migrate.latest()
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+    .then( () => {
+      return Promise.all([
+        database('foods').insert({name:"bannana", calories: 150, id:1}),
+        database('foods').insert({name:"apple", calories: 200, id:2}),
+        database('foods').insert({name:"pear", calories: 50, id:3}),
+        database('foods').insert({name:"bagel", calories: 400, id:4})
+      ])
+    })
+    .then( ()=>{
+      return Promise.all([
+        database('meal_foods').insert({meal_id:1, food_id:1}, 'id'),
+        database('meal_foods').insert({meal_id:1, food_id:2}, 'id'),
+        database('meal_foods').insert({meal_id:2, food_id:2}, 'id'),
+        database('meal_foods').insert({meal_id:2, food_id:3}, 'id'),
+        database('meal_foods').insert({meal_id:4, food_id:1}, 'id'),
+        database('meal_foods').insert({meal_id:4, food_id:2}, 'id'),
+      ])
+    })
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
   describe('get /api/v1/meals', ()=>{
     it('should return the meal object and all associated foods', (done)=>{
       chai.request(app)
@@ -70,9 +72,9 @@ describe('meal requests', ()=>{
       .post('/api/v1/meals/3/foods/4')
       .end( (err, response)=>{
         response.status.should.equal(201)
-        response.body.should.be.json
+        // response.body.should.be.json
         response.body.should.have.property('message')
-        response.body.message.should.equal("Successfully added bagel to Snack")
+        response.body.message.should.equal("Successfully added bagel to Lunch")
         done();
       })
     })
