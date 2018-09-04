fe45c5c7e546bcc3a7b7392f1eb822d153772aee
zdcthomas <zdcthomas@yahoo.com>
2018-09-04 09:28:57 -0600

    tests iteration 1 endpoint

---
+++ test/iteration_1.spec.js
@@ -0,0 +1,72 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+chai.use(chaiHttp);
+
+
+describe('API Route end points', () => {
+
+  before((done) => {
+    database.migrate.latest()
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+    .then( () => {
+      return Promise.all([
+        database('foods').insert({name:"Bannana", calories: 200, id:1}),
+        database('foods').insert({name:"Meatloaf", calories: 800, id:2}),
+        database('foods').insert({name:"Pear", calories: 50, id:3}),
+        database('foods').insert({name:"Bagel", calories: 400, id:4})
+      ])
+    })
+    .then( ()=>{
+      return Promise.all([
+        database('meal_foods').insert({meal_id:1, food_id:1}, 'id'),
+        database('meal_foods').insert({meal_id:1, food_id:2}, 'id'),
+        database('meal_foods').insert({meal_id:2, food_id:2}, 'id'),
+        database('meal_foods').insert({meal_id:2, food_id:3}, 'id'),
+        database('meal_foods').insert({meal_id:4, food_id:1}, 'id'),
+        database('meal_foods').insert({meal_id:4, food_id:4}, 'id'),
+      ])
+    })
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+  
+  describe('favorite_foods', ()=>{
+    it('should return the most eaten food(s) (foods which appear in the highest number of meals) with the times eaten, and an array of the foods with name an calories', (done)=>{
+      chai.request(app)      
+      .get('/api/v1/favorite_foods')
+      .end((err,response)=>{
+        response.should.have.status(200)
+        response.should.be.json;
+        response.body.timesEaten.should.equal(2)
+        foods = response.body.foods
+        foods.should.be.a('array')
+        foods.length.should.equal(2)
+        foods[0].should.be.a('object')
+        foods[0].name.should.equal('Banana')
+        foods[0].calories.should.equal(200)
+        foods[1].name.should.equal('Meatloaf')
+        foods[1].calories.should.equal(800)
+        done();
+      })
+    })
+  })
+  
+});
